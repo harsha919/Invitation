6b20555ec13e10ccbbb8ce3b8358803e907558bb
carrie919 <harshasunandh919@gmail.com>
2022-11-10 17:20:13 +0530

    res-te-90

--- src/App.js
+++ src/App.js
@@ -11,34 +11,48 @@ import flora from "./assets/flora.png";
 
 function App() {
   const isMobile = useMediaQuery("(max-width:600px)");
-  
-  return (
-    <Paper
-      elevation={10}
-      sx={{
-        maxWidth: "900px",
-        margin: isMobile ? "50px 20px" : "auto",
-        position: 'relative',
-        mt: "50px",
-        height: 'calc(100vh - 100px)',
-        padding: "30px" 
-      }}>
 
-      <Box sx={{ maxWidth: "600px", margin: "auto", paddingTop: "4rem" }}>
-        <Contents />
-      </Box>
+  return (
+    <div style={{ position: "relative" }}>
       <Box
+        height="100px"
+        width="100%"
         sx={{
-          position: 'absolute',
-          left: '0px',
-          top: '0px',
-          width: '100%',
-          height: '100%',
-          backgroundImage: `url(${flora})`
+          position: "fixed",
+          top: "0px",
+          left: "0px",
+          backgroundColor: "#FE036A"
         }}
-      >
-      </Box>
-    </Paper>
+        />
+      <Paper
+        elevation={10}
+        sx={{
+          maxWidth: "900px",
+          maxHeight: "900px",
+          margin: isMobile ? "50px 20px" : "auto",
+          position: 'relative',
+          mt: isMobile ? "20px" : "50px",
+          height: 'calc(100vh - 100px)',
+          padding: "30px",
+          zIndex: "1300",
+        }}>
+
+        <Box sx={{ maxWidth: "600px", margin: "auto", paddingTop: "4rem" }}>
+          <Contents />
+        </Box>
+        <Box
+          sx={{
+            position: 'absolute',
+            left: '0px',
+            top: '0px',
+            width: '100%',
+            height: '100%',
+            backgroundImage: `url(${flora})`
+          }}
+        >
+        </Box>
+      </Paper>
+    </div>
   );
 }
 
